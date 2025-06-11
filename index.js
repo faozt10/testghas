bfa2dc52b2a8dc72e4b1dd85de4d032eff0b20e2
faozt10 <faozt10@gmail.com>
2025-06-11 06:15:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-console.log("Hello, World!");
-<script\x09type="text/javascript">javascript:alert(1);</script>
-919487d945284d9cadb7c3597b7846701bd801ce
+import javascript
 
+from Expr e
+where e.isPure() and e.getParent() instanceof ExprStmt
+select e, "This expression has no effect."
