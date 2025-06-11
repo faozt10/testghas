ecdca3317b8c0c077480ac8d459a8aad5f573a08
faozt10 <faozt10@gmail.com>
2025-06-11 07:47:15 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-// Vulnerable to reflected XSS
+// Vulnerable to reflected XSSddd
 const userInput = location.hash.slice(1);  // Takes untrusted input from URL fragment
 document.write(userInput);                  // Writes input directly to the page without sanitization
