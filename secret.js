77804ab03ace0f67ff10cc0b01a43e3c7f4956ad
faozt10 <faozt10@gmail.com>
2025-06-12 07:42:03 -0700

    Update secret.js

--- secret.js
+++ secret.js
@@ -2,14 +2,10 @@
 
 // WARNING: This is an sssinsecure example with a shardcodsdfed secret for demonstration only!
 
-const apiKey = "sk_live_1234567890abcdexf1234h567890abcdef";  // Exampjle secret API key
-const apiKey = "ap_live_9999999999abcdexf1234h567890abcdef";  // Example secret API key
-const apiKey = "np_live_1111111111abcdexf1234h567890abcdef";  // Example secret API key
-const apiKey = "tk_live_44ddds333abcdexf1234h567890abcdef";  // Example secret API key
-const apiKey = "ap_live_166678907554abcdexf1234h567890abcdef";  // Example secret API key
+const Token = "sk_live_1234567890abcdexf1234h567890abcdef";  // Exampjle secret API key
 
 function fetchUserData(userId) {
-  const url = `https://api.example.com/users/${userId}?api_key=${apiKey}`;
+  const url = `https://api.example.com/users/${userId}?api_key=${Token}`;
 
   fetch(url)
     .then(response => response.json())
